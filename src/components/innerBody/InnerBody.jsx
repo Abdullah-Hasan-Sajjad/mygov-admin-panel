99ca42e4a2439c3e414679666a2aa1d64af6353a
Abdullah Hasan Sajjad Rafi <rafiabdullahhasansajjad@gmail.com>
2024-03-13 14:04:23 +0600

    inner portion updated

--- src/components/innerBody/InnerBody.jsx
+++ src/components/innerBody/InnerBody.jsx
@@ -4,37 +4,46 @@ import services from "../../data/services.json";
 import FullSelectionPortion from "../FullSelectionPortion";
 import Table from "../Table";
 import abedon from "../../data/abedon.json"
+import daak from "../../data/daak.json";
 
-export default function InnerBody(){
-    return(
+export default function InnerBody() {
+    return (
         <div className="flex flex-col bg-inner-body md:ml-64 h-full gap-4 p-4">
             <FullSelectionPortion />
 
             {/* Dashcard portion */}
             <div className="flex flex-row items-center gap-4">
                 {
-                    services.map((service, index)=>{
-                        return <Dashcard key={index} title={service.serviceName} value={service.value}/>
+                    services.map((service, index) => {
+                        return <Dashcard key={index} title={service.serviceName} value={service.value} />
                     })
                 }
             </div>
 
             {/* Abedon Table */}
-            <Table 
-            columnNames={[
-                "ক্রমিক",
-                "সেবার নাম",                
-                "অপেক্ষমান " ,               
-                "ডাকে চলমান",                
-                "নথিতে চলমান",
-                "নিষ্পত্তিকৃত" ,               
-                "মোট আবেদন"
-            ]}
-            data={abedon}/>
+            <Table
+                columnNames={[
+                    "ক্রমিক",
+                    "সেবার নাম",
+                    "অপেক্ষমান ",
+                    "ডাকে চলমান",
+                    "নথিতে চলমান",
+                    "নিষ্পত্তিকৃত",
+                    "মোট আবেদন"
+                ]}
+                data={abedon} />
 
             {/* Dak Table */}
-            <Table columnNames={[]} data={data}/>
-
+            <Table
+                columnNames={[
+                    "ক্রমিক",
+                    "সেবার নাম",
+                    "ডাকে চলমান",
+                    "নথিতে চলমান",
+                    "নিষ্পত্তিকৃত",
+                    "মোট আবেদন"
+                ]}
+                data={daak} />
 
         </div>
     );
