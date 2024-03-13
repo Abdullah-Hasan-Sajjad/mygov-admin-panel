79a19b687bc946f1fdb52798b84cb84d7f084cee
Abdullah Hasan Sajjad Rafi <rafiabdullahhasansajjad@gmail.com>
2024-03-13 13:55:53 +0600

    table updated for customization

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -20,36 +20,27 @@ export default function Table({ columnNames, data }) {
                             <tbody>
                                 {
                                     data.map((values, index) => {
-                                        console.log(values)
                                         return (
                                             <tr className="border-b border-gray-200">
+
                                                 <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50">
                                                     {rowNo++}
                                                 </th>
-                                                <td className="px-6 py-4 t">
-                                                    <a href="#">{values.ServiceName}</a>
-                                                </td>
-                                                <td className="px-6 py-4 t">
-                                                    <a href="#">{values.Opekkhoman}</a>
-                                                </td>
-                                                <td className="px-6 py-4 t">
-                                                    <a href="#">{values.Dak}</a>
-                                                </td>
-                                                <td className="px-6 py-4 t">
-                                                    <a href="#">{values.Nothi}</a>
-                                                </td>
-                                                <td className="px-6 py-4 t">
-                                                    <a href="#">{values.Nisponno}</a>
-                                                </td>
-                                                <td className="px-6 py-4 t">
-                                                    <a href="#">{values.Total}</a>
-                                                </td>
+
+                                                {
+                                                    Object.values(values).map((colVal) => {
+                                                        return (
+                                                            <td className="px-6 py-4 t">
+                                                                <a href="#">{colVal}</a>
+                                                            </td>
+                                                        )
+                                                    })
+
+                                                }
                                             </tr>
                                         )
                                     })
                                 }
-
-
                             </tbody>
                         </table>
                     </div>
