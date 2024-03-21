686c4b9ed8f025d6b48c32901f1152b88cb1a18f
Abdullah Hasan Sajjad Rafi <rafiabdullahhasansajjad@gmail.com>
2024-03-21 14:07:00 +0600

    selection portion design update

--- src/components/FullSelectionPortion.jsx
+++ src/components/FullSelectionPortion.jsx
@@ -4,11 +4,11 @@ import Selector from "./Selector";
 export default function FullSelectionPortion({label}){
     return(
         <div className="w-full mb-6 flex justify-between bg-[#bcd9c5] px-6 py-3">
-            <div className="grid grid-cols-12 items-center justify-center w-full gap-3 md:gap-10">
-                <div className="col-span-2 text-right">
+            <div className="flex flex-row items-center justify-center w-full gap-3 md:gap-10">
+                <div className="text-right">
                     <label>{label}</label>
                 </div>
-                <div className="col-span-10">
+                <div className="grow">
                     <Selector
                     placeholder={"সেবা বাছাই করুন..."}
                     searchInputPlaceholder={"সেবা খুজুন..."} />
