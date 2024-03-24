8033e37937657cdf5b8de39f9b3e1e179a32bff7
Abdullah Hasan Sajjad Rafi <rafiabdullahhasansajjad@gmail.com>
2024-03-24 12:01:57 +0600

    error page created

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,34 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AbedonView from "./components/abedon/AbedonView"
 import DaakView from "./components/daak/DaakView"
 import NathiView from "./components/nothi/NothiView"
-import MainLayout from "./components/mainLayout/MainLayout";
+import MainLayout from "./components/mainLayout/MainLayout"
+import ErrorPage from "./components/mainComponents/ErrorPage"
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <MainLayout> <Dahsboard /></MainLayout>
-    //errorElement: <ErrorPage />,
+    element: <MainLayout> <Dahsboard /></MainLayout>,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/dashboard",
-    element: <MainLayout><Dahsboard /></MainLayout>
-    //errorElement: <ErrorPage />,
+    element: <MainLayout><Dahsboard /></MainLayout>,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/abedon",
-    element: <MainLayout><AbedonView /></MainLayout>
-    //errorElement: <ErrorPage />,
+    element: <MainLayout><AbedonView /></MainLayout>,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/daak",
-    element: <MainLayout><DaakView /></MainLayout>
-    //errorElement: <ErrorPage />,
+    element: <MainLayout><DaakView /></MainLayout>,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/nathi",
-    element: <MainLayout><NathiView /></MainLayout>
-    //errorElement: <ErrorPage />,
+    element: <MainLayout><NathiView /></MainLayout>,
+    errorElement: <ErrorPage />,
   },
 ]);
 
